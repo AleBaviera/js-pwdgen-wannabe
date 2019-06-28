86016806156e554d1f7bb4ad4baa7d47a415f353
alebaviera <baviera.alessandra@gmailcom>
2019-06-28 13:56:33 +0200

    psw gen

---
+++ myscript.js
@@ -0,0 +1,14 @@
+var nome,cognome,colore;
+
+
+nome=prompt('qual è il tuo nome?');
+
+
+cognome=prompt('e il tuo cognome?');
+
+
+
+colore=prompt('il tuo colore preferito?');
+
+
+document.getElementById('psw').innerHTML=nome+cognome+colore+19;
